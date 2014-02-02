336799668e5ddbbd0f976541f0f7adda2e5e883e
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-02-02 18:27:27 +0200

    make the example better

--- python/examples/ardubus_consumer.py
+++ python/examples/ardubus_consumer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import dbus,time
+from dbushelpers.call_cached import call_cached
  
 bus = dbus.SessionBus()
 launcher = bus.get_object('fi.hacklab.ardubus.launcher', '/fi/hacklab/ardubus/launcher')
-print launcher.list_boards()
+
+for board in launcher.list_boards():
+    print "try b = get_board('%s') and then b.method(*args)" % board
+
 
 def get_board(bname):
+    print "try also call_cached('/fi/hacklab/ardubus/%s', 'method', *args)"  % bname
     return bus.get_object("fi.hacklab.ardubus.%s" % bname, "/fi/hacklab/ardubus/%s" % bname)
+    
 
 #b = get_board('rod_control_panel')
 #l = get_board('reactor_lid')
