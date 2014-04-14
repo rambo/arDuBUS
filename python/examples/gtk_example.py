5f097add3b98ad4808495f2fb460565795ba5016
Eero af Heurlin <rambo@iki.fi>
2014-04-14 21:40:13 +0300

    slider widget

--- python/examples/gtk_example.py
+++ python/examples/gtk_example.py
@@ -4,6 +4,7 @@ import gtk
 import gobject
 import dbus
 import dbus.mainloop.glib
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 from dbushelpers.call_cached import call_cached
 
 
@@ -11,6 +12,7 @@ class example_program:
     def __init__(self, bus):
         # Boilerplate
         self.mainwindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.mainwindow.resize(300,200)
         self.mainwindow.set_title('PyGTK Example')
         self.mainwindow.connect("delete_event", self.quit)
         self.mainwindow.connect("destroy", self.quit)
@@ -19,17 +21,20 @@ class example_program:
         self.bus = bus
         self.bus.add_signal_receiver(self.alias_changed, dbus_interface = "fi.hacklab.ardubus", signal_name = "alias_change")
 
-        # Divive window to left and right halves which are vboxes        
-        hbox = gtk.HBox(homogeneous=True)
-        self.main_left_column = gtk.VBox()
-        hbox.add(self.main_left_column)
-        self.main_right_column = gtk.VBox()
-        hbox.add(self.main_right_column)
-        self.mainwindow.add(hbox)
-        
-        self.led_pwm = gtk.Adjustment(value=0, lower=0, upper=255, step_incr=1, page_incr=0, page_size=0)
+        # Divide widow to top & bottom halves
+        vbox = gtk.VBox(homogeneous=False)
+        self.top_half = gtk.HBox()
+        vbox.pack_start(self.top_half, fill=False, expand=False)
+        self.bottom_half = gtk.HBox()
+        vbox.pack_start(self.bottom_half, fill=False, expand=False)
+        self.mainwindow.add(vbox)
+
+        self.led_pwm = gtk.Adjustment(value=0, lower=0, upper=255, step_incr=1.0)
+        self.led_pwm.connect("value_changed", self.led_pwm_changed)
         self.led_pwm_slider = gtk.HScale(adjustment=self.led_pwm)
-        self.main_right_column.add(self.led_pwm_slider)
+        self.led_pwm_slider.set_digits(0)
+        self.top_half.pack_start(gtk.Label("LED brightness"), fill=False, expand=False)
+        self.top_half.pack_start(self.led_pwm_slider, fill=True, expand=True)
 
 
     def alias_changed(self, alias, value, sender):
@@ -37,11 +42,13 @@ class example_program:
         pass
 
 
-    def led_slider_changed(self, *args):
-        # TODO: read the value and set led PWM
-        pass
-        value = 0
-        call_cached('/fi/hacklab/ardubus/gtk_example_board', 'set_alias', value)
+    def led_pwm_changed(self, *args):
+        value = self.led_pwm.get_value()
+        try:
+            call_cached('/fi/hacklab/ardubus/gtk_example_board', 'set_alias', value)
+        except dbus.exceptions.DBusException,e:
+            # No arduino
+            print "Could not set value %d via arDuBUS" % value
 
 
     def quit(self, widget, data=None):
@@ -55,7 +62,6 @@ class example_program:
 
 
 if __name__ == '__main__':
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus = dbus.SessionBus()
     w = example_program(bus)
     w.mainloop()
