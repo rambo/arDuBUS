cfd1c1ded1b6acd47df470bc8e964a0f87651e24
Eero af Heurlin <rambo@iki.fi>
2014-04-14 21:56:00 +0300

    bigger text

--- python/examples/gtk_example.py
+++ python/examples/gtk_example.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""This is a very simple arDuBUS example reading one button and controlling PWM of one LED, use the provided YML to generate the Arduino code"""
 import pygtk
 import gtk
 import gobject
@@ -29,23 +30,36 @@ class example_program:
         vbox.pack_start(self.bottom_half, fill=False, expand=False)
         self.mainwindow.add(vbox)
 
+        # Make a slider widget for the LED
         self.led_pwm = gtk.Adjustment(value=0, lower=0, upper=255, step_incr=1.0)
         self.led_pwm.connect("value_changed", self.led_pwm_changed)
         self.led_pwm_slider = gtk.HScale(adjustment=self.led_pwm)
         self.led_pwm_slider.set_digits(0)
-        self.top_half.pack_start(gtk.Label("LED brightness"), fill=False, expand=False)
+        sliderlabel = gtk.Label()
+        sliderlabel.set_markup("<span size='24000'>LED brightness</span>")
+        self.top_half.pack_start(sliderlabel, fill=False, expand=False)
         self.top_half.pack_start(self.led_pwm_slider, fill=True, expand=True)
+        
+        self.button_state_text = gtk.Label()
+        self.button_state_text.set_markup("<span size='32000'>Button state: unknown</span>")
+        self.bottom_half.pack_start(self.button_state_text)
 
 
     def alias_changed(self, alias, value, sender):
-        # TODO: display state
-        pass
+        if alias != 'example_button':
+            print "Got unknown alias %s from %s (state was %s)" % (alias, sender, repr(value))
+            return
+        # Remember, inputs are pulled up and button shorts to ground
+        if value:
+            self.button_state_text.set_markup("<span size='32000'>Button state: released</span>")
+        else:
+            self.button_state_text.set_markup("<span size='32000'>Button state: pressed</span>")
 
 
     def led_pwm_changed(self, *args):
         value = self.led_pwm.get_value()
         try:
-            call_cached('/fi/hacklab/ardubus/gtk_example_board', 'set_alias', value)
+            call_cached('/fi/hacklab/ardubus/gtk_example_board', 'set_alias', 'dimmable_led', value)
         except dbus.exceptions.DBusException,e:
             # No arduino
             print "Could not set value %d via arDuBUS" % value
