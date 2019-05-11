2b8f1eb183f31cf7ac270d10889cfa19e0f0c859
Eero af Heurlin <eero.afheurlin@iki.fi>
2019-05-11 16:58:30 +0300

    Resolve aliases for events

--- python3-ardubus/ardubus_core/events.py
+++ python3-ardubus/ardubus_core/events.py
@@ -1,13 +1,16 @@
 """Event messages coming back from the serialport abstracted"""
+import logging
 
 # pylint: disable=R0903
+
+LOGGER = logging.getLogger(__name__)
 
 
 class BaseEvent:
     """baseclass for events"""
     alias = None
     idx = None
-    configkey = ''
+    _configkey = None
 
     def __init__(self, device_config_map, idx, **kwargs):
         self.idx = idx
@@ -15,8 +18,19 @@ class BaseEvent:
         if self.alias is None:
             self.alias = self.resolve_alias(device_config_map, idx, **kwargs)
 
-    def resolve_alias(self, device_config_map, idx, **kwargs):
+    def resolve_alias(self, device_config_map, idx, **kwargs):  # pylint: disable=W0613
         """Resolve the alias from config based on the given index"""
+        if self._configkey not in device_config_map:
+            LOGGER.error('self._configkey(={}) not found in device_config_map'.format(self._configkey))
+            return None
+        config = device_config_map[self._configkey]
+        try:
+            item = config[idx]
+        except (KeyError, AttributeError):
+            LOGGER.exception('Could not get item')
+            return None
+        if 'alias' in item:
+            return item['alias']
         return None
 
     def __str__(self):
@@ -39,6 +53,7 @@ class PinEvent(BaseEvent):
     """MCU digital input events"""
     pin = None
     state = False
+    _configkey = 'digital_in_pins'
 
 
 class PinChange(PinEvent, Change):
@@ -53,6 +68,7 @@ class AnalogPinEvent(BaseEvent):
     """MCU analog input events"""
     pin = None
     value = 0
+    _configkey = 'analog_in_pins'
 
 
 class AnalogPinChange(AnalogPinEvent, Change):
@@ -67,6 +83,7 @@ class PCA9535PinEvent(BaseEvent):
     """PCA953 I2C digital io-expander events"""
     pin = None
     state = False
+    _configkey = 'pca9535_inputs'
 
 
 class PCA9535PinChange(PCA9535PinEvent, Change):
