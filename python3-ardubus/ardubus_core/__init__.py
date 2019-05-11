f92192e1024cfa2588b8ce621021beaab7971a05
Eero af Heurlin <eero.afheurlin@iki.fi>
2019-05-11 17:59:15 +0300

    add loglevel to log init

--- python3-ardubus/ardubus_core/__init__.py
+++ python3-ardubus/ardubus_core/__init__.py
@@ -5,6 +5,6 @@ import logging
 DEFAULT_LOG_FORMAT = '[%(asctime)s][%(levelname)s] %(name)s (%(process)d) %(pathname)s:%(funcName)s:%(lineno)d | %(message)s'  # noqa: E501 ; # pylint: disable=C0301
 
 
-def init_logging():
+def init_logging(level=logging.INFO):
     """Initialize logging, call this if you don't know any better logging arrangements"""
-    logging.basicConfig(format=DEFAULT_LOG_FORMAT)
+    logging.basicConfig(format=DEFAULT_LOG_FORMAT, level=level)
